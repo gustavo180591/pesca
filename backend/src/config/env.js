02f8a570e1ea4b706c1630dce8a7d8704c4da84e
gustavo180591 <gustavo.faccendini@gmail.com>
2025-02-05 20:23:45 -0300

    save changes

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -4,4 +4,4 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 5000;
 export const JWT_SECRET = process.env.JWT_SECRET || 'clave-secreta';
-export const DATABASE_URL = process.env.DATABASE_URL;
+export const DATABASE_URL = process.env.DATABASE_URLl;
